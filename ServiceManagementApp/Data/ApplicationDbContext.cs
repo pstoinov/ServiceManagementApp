310584b5824e9bf894fc87ad77797e839a1d9bd0
Plamen Stoynov <126239974+pstoinov@users.noreply.github.com>
2024-08-22 17:07:02 +0300

    added docker compse support

--- ServiceManagementApp/Data/ApplicationDbContext.cs
+++ ServiceManagementApp/Data/ApplicationDbContext.cs
@@ -1,5 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using ServiceManagementApp.Data.Models.ClientModels;
+using ServiceManagementApp.Data.Models.Core;
+using ServiceManagementApp.Data.Models.RepairModels;
+using ServiceManagementApp.Data.Models.RequestModels;
+using ServiceManagementApp.Data.Models.ServiceModels;
+using ServiceManagementApp.Data.Models.Wherehouse;
 
 namespace ServiceManagementApp.Data
 {
@@ -10,7 +16,18 @@ namespace ServiceManagementApp.Data
         {
         }
 
-       
-        // public DbSet<YourModel> YourModels { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<CashRegister> CashRegisters { get; set; }
+        public DbSet<CashRegisterRepair> CashRegisterRepairs { get; set; }
+        public DbSet<Repair> Repairs { get; set; }
+        public DbSet<Contract> Contracts { get; set; }
+        public DbSet<Phone> Phones { get; set; }
+        public DbSet<Email> Emails { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<ServiceRequest> ServiceRequests { get; set; }
+        public DbSet<Part> Parts { get; set; }
     }
 }
