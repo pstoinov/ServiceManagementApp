b3f18e79eb7f5be4ec51062ee050ed6ca83c8607
Plamen Stoynov <126239974+pstoinov@users.noreply.github.com>
2024-09-02 17:35:18 +0300

    UI: Minor UI Changes

---
+++ ServiceManagementApp/Controllers/Manager.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceManagementApp.Controllers
+{
+    public class Manager : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
