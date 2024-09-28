fd40357851a525c0cfdb06500c1dfddcf3f3ea49
Plamen Stoynov <126239974+pstoinov@users.noreply.github.com>
2024-09-28 23:48:22 +0300

    CORE: Add EmployeeService

---
+++ ServiceManagementApp/Services/EmployeeService.cs
@@ -0,0 +1,20 @@
+﻿using ServiceManagementApp.Data.Models.ServiceModels;
+using ServiceManagementApp.Data;
+using Microsoft.EntityFrameworkCore;
+
+public class EmployeeService
+{
+    private readonly ApplicationDbContext _context;
+
+    public EmployeeService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Employee>> GetEmployeesByServiceId(int serviceId)
+    {
+        return await _context.Employees
+            .Where(e => e.ServiceId == serviceId)
+            .ToListAsync();
+    }
+}
